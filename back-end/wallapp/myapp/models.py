be78340ac2a909c9896f3ba56248bfb6ace61b7c
joaorfp <joaorfp12345@gmail.com>
2023-01-31 13:21:36 -0300

    setting up the CRUD and other settings

--- back-end/wallapp/myapp/models.py
+++ back-end/wallapp/myapp/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
+class Users(models.Model):
+  name = models.CharField(max_length=20)
+  email = models.CharField(max_length=50)
+  password = models.CharField(max_length=20)
+  role = models.CharField(max_length=20)
+
+
 # Create your models here.
--- back-end/wallapp/myapp/views.py
+++ back-end/wallapp/myapp/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from rest_framework import generics
+from .models import Users
+from .serializers import UsersSerializer
 
+class CreateUserView(generics.CreateAPIView):
+  queryset = Users.objects.all()
+  serializer_class = UsersSerializer
+
+class GetUsersView(generics.ListAPIView):
+  queryset = Users.objects.all()
+  serializer_class = UsersSerializer
+
+class UpdateUserView(generics.UpdateAPIView):
+  queryset = Users.objects.all()
+  serializer_class = UsersSerializer
+
+class DeleteUserView(generics.DestroyAPIView):
+  queryset = Users.objects.all()
+  serializer_class = UsersSerializer
 # Create your views here.
